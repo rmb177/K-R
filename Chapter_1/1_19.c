6dec5567d5759173f3e5361ac789169bd5ec87a3
Ryan Brubaker <ryan.brubaker@gmail.com>
2011-12-15 21:50:37 -0600

    Fixed some issues in 1_18. Added solution for 1_19

---
+++ Chapter_1/1_19.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define MAXLINE 1000	/* maximum line size */
+int GetReversedLine(char line[], int maxline);
+void ReverseLine(char line[], int length);
+
+/**
+ * Print all lines reversed
+ */
+main()
+{
+   int len;
+   char line[MAXLINE];
+   
+   int max = 0;
+   while ((len = GetLine(line, MAXLINE)) > 0)
+   {
+      printf("Reversed line: %s\n\n", line);
+   }
+   return 0;
+}
+
+/**
+ * Read a line into s and return length
+ */ 
+int GetLine(char s[], int lim)
+{
+   int c;
+   int i;
+   
+   /* There was a bug in original K&R code, need to save two spaces for */
+   /* new line and null character */
+   for (i = 0; i < lim - 2 && (c = getchar()) != EOF && c != '\n'; ++i)
+   {
+      s[i] = c;
+   }
+   s[i] = '\0';
+
+   /* Don't need to reverse empty strings or strings with 1 character */
+   if (i > 1)
+   {
+      ReverseLine(s, i);
+   }
+   return i;
+}
+
+
+/**
+ * Reverse the given line
+ */
+void ReverseLine(char line[], int length)
+{
+   printf("%d\n", length);
+   int startIndex = 0;
+   int endIndex = length - 1;
+
+   while (startIndex < endIndex)
+   {
+      char temp = line[startIndex];
+      line[startIndex++] = line[endIndex];
+      line[endIndex--]  = temp;
+   }
+}
+
